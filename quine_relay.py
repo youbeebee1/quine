30f75676e984b73ce28e12a0ad7705f0d5402b9e
youbeebee <youbeebee@gmail.com>
2026-07-25 17:19:35 +0900

    줄바꿈 제거

--- quine_relay.py
+++ quine_relay.py
@@ -1,6 +1,6 @@
 # 7 langs quine relay (python -> c -> js -> cpp -> java -> ruby -> rust -> python) (c) UBB
 import json
-s = '# 7 langs quine relay (python -> c -> js -> cpp -> java -> ruby -> rust -> python) (c) UBB\nimport json\ns = %r\nc = %r\njs = %r\ncpp = %r\njava = %r\nruby = %r\nrust = %r\npython_code = s %% (s, c, js, cpp, java, ruby, rust)\nrust_code = rust %% json.dumps(python_code)\nruby_code = ruby %% json.dumps(rust_code)\njava_code = java %% json.dumps(ruby_code)\ncpp_code = cpp %% json.dumps(java_code)\njs_code = js %% json.dumps(cpp_code)\nc_code = c %% json.dumps(js_code)\nprint(c_code)\n'
+s = '# 7 langs quine relay (python -> c -> js -> cpp -> java -> ruby -> rust -> python) (c) UBB\nimport json\ns = %r\nc = %r\njs = %r\ncpp = %r\njava = %r\nruby = %r\nrust = %r\npython_code = s %% (s, c, js, cpp, java, ruby, rust)\nrust_code = rust %% json.dumps(python_code)\nruby_code = ruby %% json.dumps(rust_code)\njava_code = java %% json.dumps(ruby_code)\ncpp_code = cpp %% json.dumps(java_code)\njs_code = js %% json.dumps(cpp_code)\nc_code = c %% json.dumps(js_code)\nprint(c_code)'
 c = '#include <stdio.h>\nint main() {\n    char s[] = %s;\n    printf("%%s", s);\n}'
 js = 'const s = %s;\nconsole.log(s);'
 cpp = '#include <iostream>\n#include <string>\nint main() {\n    std::string s = %s;\n    std::cout << s;\n}'
